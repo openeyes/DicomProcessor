bb2b411d86215953e12dc454772b71352600b343
adrianbrenton <adrian.r.brenton@gmail.com>
2019-03-01 15:05:36 +0000

    Move methods from Study to PDFUtils for Single Responsibility Principle

--- src/main/java/com/abehrdigital/dicomprocessor/Study.java
+++ src/main/java/com/abehrdigital/dicomprocessor/Study.java
@@ -1,16 +1,19 @@
 package com.abehrdigital.dicomprocessor;
 
-import com.abehrdigital.dicomprocessor.utils.PDFUtils;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.dcm4che3.data.Sequence;
 
 import javax.sql.rowset.serial.SerialBlob;
-import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Map;
 
+/**
+ * Represents a Dicom file and its associated data.
+ *
+ * @author Adrian Brenton
+ */
 public class Study {
 
     private PDDocument pdfDoc;
@@ -22,6 +25,7 @@ public class Study {
 
     }
 
+    //TODO: for SRP, move this somewhere else
     public String getHeaderJsonString() {
         Gson gson = new GsonBuilder().disableHtmlEscaping().setPrettyPrinting().create();
         return gson.toJson(nonSequenceDicomElements);
@@ -47,27 +51,5 @@ public class Study {
     //Simple elements includes non-pdf and non-sequence elements only
     public void setSimpleDicomElements(Map<Integer, String> elements) {
         nonSequenceDicomElements = elements;
-    }
-
-    public void setSequenceDicomElements() {
-
-    }
-
-    public void savePdf(String filepath) {
-        PDFUtils.savePdf(pdfDoc, filepath);
-    }
-
-    public String extractTextFromPage(double x1, double x2, double x3, double x4, int pageIndex) throws IOException {
-        return PDFUtils.getTextFromRectangle(x1, x2, x3, x4, pdfDoc.getPage(pageIndex));
-    }
-
-    // for testing - do not use in final project
-    // TODO: consider using Linked HashMap to maintain order
-    public String dumpData() {
-        String output = "";
-        for (Map.Entry<Integer, String> entry : nonSequenceDicomElements.entrySet()) {
-            output += entry.getKey().toString() + " " + entry.getValue() + "\n";
-        }
-        return output;
     }
 }
