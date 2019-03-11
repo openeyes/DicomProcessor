fd22ddbfad200734b40f1ff787a73737cb9ead36
Stefan Toncu <stefan.toncu29@gmail.com>
2019-03-11 14:20:24 +0000

    dicomProcessor/jsonActionParser - fix bug in delink routine, where the current event_attachment_item must replace the previous one

--- src/main/java/com/abehrdigital/dicomprocessor/dao/EventAttachmentItemDao.java
+++ src/main/java/com/abehrdigital/dicomprocessor/dao/EventAttachmentItemDao.java
@@ -3,6 +3,8 @@ package com.abehrdigital.dicomprocessor.dao;
 import org.hibernate.Session;
 import org.hibernate.query.NativeQuery;
 
+import java.math.BigInteger;
+
 public class EventAttachmentItemDao {
     private Session session;
 
@@ -10,16 +12,30 @@ public class EventAttachmentItemDao {
         this.session = session;
     }
 
-    public void deleteByAttachmentDataId(int attachmentDataId){
+    public void deleteByAttachmentDataId(int attachmentDataId) {
         NativeQuery query = session.createSQLQuery("" +
-                "DELETE FROM event_attachment_item " +
-                "WHERE attachment_data_id = :attachment_data_id AND " +
-                "id != ( SELECT * FROM ( " +
-                "SELECT MAX(id) FROM event_attachment_item " +
-                        " WHERE attachment_data_id = :attachment_data_id" +
-                        " ) AS eai ) ")
+                "SELECT COUNT(id) FROM `event_attachment_item` " +
+                "WHERE `attachment_data_id`= :attachment_data_id")
                 .setParameter("attachment_data_id", attachmentDataId);
+        int numberRecordsForAttachmentDataId = ((BigInteger) query.list().get(0)).intValue();
 
+        if (numberRecordsForAttachmentDataId > 1) {
+            System.err.println(">1");
+            query = session.createSQLQuery("" +
+                    "DELETE FROM event_attachment_item " +
+                    "WHERE attachment_data_id = :attachment_data_id AND " +
+                    "id != ( SELECT * FROM ( " +
+                    "SELECT MAX(id) FROM event_attachment_item " +
+                    " WHERE attachment_data_id = :attachment_data_id" +
+                    " ) AS eai ) ")
+                    .setParameter("attachment_data_id", attachmentDataId);
+        } else {
+            System.err.println("==1");
+            query = session.createSQLQuery("" +
+                    "DELETE FROM event_attachment_item " +
+                    "WHERE event_attachment_item.attachment_data_id = :attachment_data_id ")
+                    .setParameter("attachment_data_id", attachmentDataId);
+        }
         query.executeUpdate();
     }
 }
