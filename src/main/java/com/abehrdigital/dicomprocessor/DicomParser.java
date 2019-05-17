30e143c5fe9f4ed85aac414b2aae9b8f14a59a35
Justinas Cepkauskas <justinas.cepkauskas@abehr.com>
2019-05-17 11:10:06 +0100

    Refactor DicomParser to not put really big files in the json headers and pass the dicom blob to the study and remove image processing in the run function

--- src/main/java/com/abehrdigital/dicomprocessor/DicomParser.java
+++ src/main/java/com/abehrdigital/dicomprocessor/DicomParser.java
@@ -4,10 +4,6 @@ package com.abehrdigital.dicomprocessor;
 import org.dcm4che3.data.Attributes;
 import org.dcm4che3.io.DicomInputStream;
 
-import javax.imageio.ImageIO;
-import javax.imageio.stream.ImageInputStream;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.sql.Blob;
@@ -19,20 +15,14 @@ public class DicomParser {
     private Study study;
     private Blob dicomBlob;
     private DicomInputStream dicomInputStream;
-    private static final String imageFormat = "png";
     private Attributes attributes;
-    private int pdfTagNumber;
+    private byte[] attachmentBytes;
+    private static final int MAXIMUM_SIMPLE_ELEMENT_BYTE_LENGTH = 1000;
 
-    public DicomParser(Blob dicomBlob) throws Exception {
+    public DicomParser(Blob dicomBlob, Study study) throws Exception {
         this.dicomBlob = dicomBlob;
-        //this.file = dicomFile;
-        this.study = new Study(); //TODO: consider injecting this dependency
-        this.pdfTagNumber = 4325393; // TODO: change so that this is not hardcoded
-        try {
-            run();
-        } catch (Exception exception) {
-            throw new Exception(exception);
-        }
+        this.study = study;
+        run();
     }
 
     private void init() throws IOException, SQLException {
@@ -43,16 +33,18 @@ public class DicomParser {
     private Map<String, String> simpleElements() {
         Map<String, String> elements = new HashMap<>();
         for (int tag : attributes.tags()) {
-            if (tag != pdfTagNumber) {
-                byte[] valueAsByte;
-                try {
-                    valueAsByte = (byte[]) attributes.getValue(tag); //casted because getValue() returns Object
-                } catch (ClassCastException ex) {
-                    //TODO: handle cases where value is a sequence
-                    continue;
-                }
+            byte[] valueAsByte;
+            try {
+                valueAsByte = (byte[]) attributes.getValue(tag); //casted because getValue() returns Object
+            } catch (ClassCastException ex) {
+                //TODO: handle cases where value is a sequence
+                continue;
+            }
+            if (valueAsByte.length < MAXIMUM_SIMPLE_ELEMENT_BYTE_LENGTH) {
                 String value = new String(valueAsByte, Charset.forName("UTF-8"));
                 elements.put(Integer.toHexString(tag), value.trim());
+            } else {
+                attachmentBytes = valueAsByte;
             }
         }
         return elements;
@@ -60,15 +52,9 @@ public class DicomParser {
 
     public void run() throws IOException, SQLException {
         init();
-        // Put set study pdf member variables:
-        byte[] pdfBytes = attributes.getBytes(pdfTagNumber);
-        ImageInputStream inputStream = javax.imageio.ImageIO.createImageInputStream(dicomBlob.getBinaryStream());
-        BufferedImage image = ImageIO.read(inputStream);
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ImageIO.write(image, imageFormat, baos);
-        study.setImageFields(baos.toByteArray());
-        study.setPdfFields(pdfBytes);
+        study.setDicomBlob(dicomBlob);
         study.setSimpleDicomElements(simpleElements());
+        study.setAttachmentBytes(attachmentBytes);
     }
 
     public Study getStudy() {
