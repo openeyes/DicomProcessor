055b4cdb35cf6df6367ddffd66b03ebc3b5fe2dc
Justin <justinas.cepkauskas@gmail.com>
2019-11-25 08:08:41 +0000

    Fix regex to not convert object methods

--- src/main/java/com/abehrdigital/payloadprocessor/utils/JavascriptRoutineMethodConverter.java
+++ src/main/java/com/abehrdigital/payloadprocessor/utils/JavascriptRoutineMethodConverter.java
@@ -1,22 +1,15 @@
 package com.abehrdigital.payloadprocessor.utils;
 
-import com.abehrdigital.payloadprocessor.RequestWorker;
-
 import java.lang.reflect.Method;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.regex.Pattern;
 
-import static java.util.regex.Pattern.compile;
 
 public class JavascriptRoutineMethodConverter {
 
     public static String convertScriptJavaMethodsWithClassPrefix(String routineScript, Method[] methods, String prefix) {
         String convertedScript = routineScript;
         for (Method method : methods) {
-            Logger.getLogger(RequestWorker.class.getName()).log(Level.SEVERE, Pattern.compile("^(?![.])" + method.getName() + "\\(").toString());
             convertedScript = convertedScript.replaceAll(
-                    "([?:\\s(])?(?![.])" + method.getName() + "\\(",
+                    "(?<![.])" + method.getName() + "\\(",
                     prefix + "." + method.getName() + "("
             );
         }
